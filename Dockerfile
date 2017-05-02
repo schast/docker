43c254009b81fe4a3ea67befa786cd0e911e9629
Guenter Bailey <office@bailey-solution.com>
2017-05-02 21:04:08 +0200

    initial commit for new Project

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM debian:stretch
+FROM alpine:3.5
 MAINTAINER Guenter Bailey
 
 # since docker v1.9, we can use --build-arg variable
@@ -7,37 +7,34 @@ ARG branch
 ENV ADM_BRANCH=${branch:-master}
 
 # install required packages
-RUN apt-get update
-RUN apt-get install -y apache2 libapache2-mod-php php php-common php-mcrypt php-mysql php-cli php-gd zip unzip gzip php-pgsql git php7.0-xml php7.0-zip
+RUN apk update
+RUN apk add apache2 php7-apache2 php7 php7-common php7-mcrypt php7-mysqli php7-pgsql php7-gd php7-xml php7-zip git zip unzip gzip
 
 # set enviroments
 ENV GITURL="https://github.com/Admidio/admidio.git"
-ENV APACHECONF="/etc/apache2/sites-available"
+ENV APACHECONF="/etc/apache2"
 ENV WWW="/var/www"
 ENV ADM="admidio"
 ENV PROV="provision"
 
-COPY admidio_apache.conf $APACHECONF/"admidio.conf"
+COPY admidio_apache.conf $APACHECONF/"admidio_apache.conf"
+RUN echo "Include $APACHECONF/admidio_apache.conf" >> $APACHECONF/httpd.conf
 COPY entrypoint.sh /"entrypoint.sh"
 
 WORKDIR $WWW
-RUN a2dissite 000-default.conf && a2ensite admidio.conf
 
 #Admidio Git
 RUN echo "Clone Admidio from GiT with Branch $ADM_BRANCH" && \
 git clone --depth 1 --single-branch --branch $ADM_BRANCH https://github.com/Admidio/admidio.git $ADM && \
-chown -R www-data:www-data $ADM && \
-chmod -R 777 $ADM/adm_my_files && \
-apt-get autoremove -y && \
-apt-get autoclean -y && \
-apt-get clean
+chown -R apache:apache $ADM && \
+chmod -R 777 $ADM/adm_my_files
 
 #create prov folder
 RUN mkdir -p $PROV && \
 cp -a $ADM/adm_my_files $ADM/adm_plugins $ADM/adm_themes $PROV/
 
-RUN sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 30M/g" /etc/php/7.0/apache2/php.ini
-RUN sed -i "s/post_max_size = 8M/post_max_size = 40M/g" /etc/php/7.0/apache2/php.ini
+RUN sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 30M/g" /etc/php7/php.ini
+RUN sed -i "s/post_max_size = 8M/post_max_size = 40M/g" /etc/php7/php.ini
 
 VOLUME ["$WWW/$ADM/adm_my_files", "$WWW/$ADM/adm_themes", "$WWW/$ADM/adm_plugins" ,"$APACHECONF"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,37 +1,40 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-: "${APACHE_CONFDIR:=/etc/apache2}"
-: "${APACHE_ENVVARS:=$APACHE_CONFDIR/envvars}"
-if test -f "$APACHE_ENVVARS"; then
-	. "$APACHE_ENVVARS"
-fi
+# Apache gets grumpy about PID files pre-existing
+rm -f /usr/local/apache2/logs/httpd.pid
 
-: "${APACHE_RUN_DIR:=/var/run/apache2}"
-: "${APACHE_PID_FILE:=$APACHE_RUN_DIR/apache2.pid}"
-rm -f "$APACHE_PID_FILE"
-
-# create missing directories
-# (especially APACHE_RUN_DIR, APACHE_LOCK_DIR, and APACHE_LOG_DIR)
-for e in "${!APACHE_@}"; do
-	if [[ "$e" == *_DIR ]] && [[ "${!e}" == /* ]]; then
-		# handle "/var/lock" being a symlink to "/run/lock", but "/run/lock" not existing beforehand, so "/var/lock/something" fails to mkdir
-		#   mkdir: cannot create directory '/var/lock': File exists
-		dir="${!e}"
-		while [ "$dir" != "$(dirname "$dir")" ]; do
-			dir="$(dirname "$dir")"
-			if [ -d "$dir" ]; then
-				break
-			fi
-			absDir="$(readlink -f "$dir" 2>/dev/null || :)"
-			if [ -n "$absDir" ]; then
-				mkdir -p "$absDir"
-			fi
-		done
-
-		mkdir -p "${!e}"
-	fi
-done
+#~ : "${APACHE_CONFDIR:=/etc/apache2}"
+#~ : "${APACHE_ENVVARS:=$APACHE_CONFDIR/envvars}"
+#~ if test -f "$APACHE_ENVVARS"; then
+	#~ . "$APACHE_ENVVARS"
+#~ fi
+#~ 
+#~ : "${APACHE_RUN_DIR:=/var/run/apache2}"
+#~ : "${APACHE_PID_FILE:=$APACHE_RUN_DIR/apache2.pid}"
+#~ rm -f "$APACHE_PID_FILE"
+#~ 
+#~ # create missing directories
+#~ # (especially APACHE_RUN_DIR, APACHE_LOCK_DIR, and APACHE_LOG_DIR)
+#~ for e in "${!APACHE_@}"; do
+	#~ if [[ "$e" == *_DIR ]] && [[ "${!e}" == /* ]]; then
+		#~ # handle "/var/lock" being a symlink to "/run/lock", but "/run/lock" not existing beforehand, so "/var/lock/something" fails to mkdir
+		#~ #   mkdir: cannot create directory '/var/lock': File exists
+		#~ dir="${!e}"
+		#~ while [ "$dir" != "$(dirname "$dir")" ]; do
+			#~ dir="$(dirname "$dir")"
+			#~ if [ -d "$dir" ]; then
+				#~ break
+			#~ fi
+			#~ absDir="$(readlink -f "$dir" 2>/dev/null || :)"
+			#~ if [ -n "$absDir" ]; then
+				#~ mkdir -p "$absDir"
+			#~ fi
+		#~ done
+#~ 
+		#~ mkdir -p "${!e}"
+	#~ fi
+#~ done
 
 # check if Admidio directory is empty.
 # when empty then copy files from prov folder
@@ -41,17 +44,18 @@ do
     echo "directory is Empty"
     echo "copy from prov $folder"
     cp -a $WWW/$PROV/$folder/* $WWW/$ADM/$folder/
-    chown -R www-data:www-data $WWW/$ADM/$folder
+    chown -R apache:apache $WWW/$ADM/$folder
 	fi
 done
 
 # patch
 # AH00558: apache2: Could not reliably determine the server's fully qualified domain name, using <docker_ip> Set the 'ServerName' directive globally to suppress this message
-APACHE_CONF="/etc/apache2/conf-available"
-APACHE_CONF_ENABLE="/etc/apache2/conf-enabled"
+APACHE_CONF="/etc/apache2/conf.d"
+#APACHE_CONF_ENABLE="/etc/apache2/conf-enabled"
 if [ ! -f "$APACHE_CONF/fqdn.conf" ]
 then
-	echo "ServerName   `hostname`" > "$APACHE_CONF/fqdn.conf" && ln -s "$APACHE_CONF/fqdn.conf" "$APACHE_CONF_ENABLE/fqdn.conf"
+	echo "ServerName   `hostname`" > "$APACHE_CONF/fqdn.conf"
 fi
 
-exec apache2 -DFOREGROUND "$@"
+#exec apache2 -DFOREGROUND "$@"
+exec httpd -DFOREGROUND "$@"
